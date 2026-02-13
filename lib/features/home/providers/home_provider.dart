171da9b112fab8d43fcde5736a91daaccdaf57c9
Leader <jhuth981@gmail.com>
2026-02-13 15:01:01 +0300

    Home/implement HomeProvider And Use RequestState enum.

---
+++ lib/features/home/providers/home_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:news/core/enums/requst_state_enum.dart';
+import 'package:news/features/home/models/news_article_model.dart';
+import 'package:news/features/home/repos/home_repositrey.dart';
+
+class HomeProvider extends ChangeNotifier {
+  final IHomeRepository homeRepository;
+  HomeProvider({required this.homeRepository});
+
+  List<NewsArticleModel> _topHeadlinesArticles = [];
+  List<NewsArticleModel> get topHeadlinesArticles => _topHeadlinesArticles;
+
+  List<NewsArticleModel> _everythingArticles = [];
+  List<NewsArticleModel> get everythingArticles => _everythingArticles;
+
+  RequestState _requestState = RequestState.loading;
+  RequestState get requestState => _requestState;
+
+  String _errorMessage = "";
+  String get errorMessage => _errorMessage;
+
+  Future<void> getTopHeadlines(String catagory) async {
+    try {
+      _requestState = RequestState.loading;
+      notifyListeners();
+      _topHeadlinesArticles = await homeRepository.getTopHeadlines(catagory);
+      _requestState = RequestState.success;
+      notifyListeners();
+    } catch (e) {
+      _requestState = RequestState.error;
+      notifyListeners();
+      _errorMessage = e.toString();
+    }
+  }
+
+  Future<void> getEverything() async {
+    try {
+      _requestState = RequestState.loading;
+      notifyListeners();
+      _everythingArticles = await homeRepository.getEverything();
+      _requestState = RequestState.success;
+      notifyListeners();
+    } catch (e) {
+      _requestState = RequestState.error;
+      notifyListeners();
+      _errorMessage = e.toString();
+    }
+  }
+}
